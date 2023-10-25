cffb14519f6b012eb12ac9d1182be3356336d3ff
iErikstoo <santiagoricaurtec@gmail.com>
2023-10-25 16:51:41 -0500

    conf(backend): add changes

---
+++ ENTREGABLE/ENTREGABLE/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ENTREGABLE.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public String Package { get; set; }
+        public bool IsDiscontinued { get; set; } = false;
+        [ForeignKey("SupplierId")]
+        public  Supplier Supplier { get; set; }
+        public ICollection<OrderItem>? OrderItems{ get; set; }
+    }
+}
